a63561c5949608d20eb7e16933632b4fc3d15db6
ppippi804 <ppippi804@naver.com>
2021-05-13 15:55:41 +0900

    git init

--- src/some/Main.java
+++ src/some/Main.java
@@ -2,6 +2,7 @@ package some;
 
 public class Main {
 public Main() {
-System.out.println("hi");	// TODO Auto-generated constructor stub
+System.out.println("hi");	
+System.out.println("nomno,,h");// TODO Auto-generated constructor stub
 }
 }
